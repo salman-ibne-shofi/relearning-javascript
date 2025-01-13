663f7839eef6c2c0215e4f36001035d0fff23645
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-01-13 20:40:27 +0600

    11 types of maths included

--- math.js
+++ math.js
@@ -2,5 +2,16 @@
  * *****************************************************
  *              MATH NEEDED FOR PROGRAMMING
  * *****************************************************
- * 
+ * 1. Number: +ve, -ve, 0 [ 50, -9, 0 ]
+ * 2. Integer: 2, 88, [odd, even]: 2, 4, 897
+ * 3. Float: 5.6, 2.2
+ * 4. Operator: +, -, *, /
+ * 5. More operation: %, [ ]
+ * 6. Percentage: 5%, 50 taka er 10% koto?
+ * 7. Sum: 14, 18, 16
+ * 8. Avg: 14, 18, 16
+ * 9. Max/Min: 14, 18, 16
+ * 10. Series: 
+ * 11. Area: Triangle
+ * 12. 
  */ 
