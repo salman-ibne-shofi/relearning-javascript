3b702eda6905451666c850c7f85e5afca5ca305b
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-01-14 22:52:53 +0600

    string type variable boolean type variable number type variable

---
+++ types.js
@@ -0,0 +1,15 @@
+// String type variable
+var name = "Sadar Uddin";
+var address = "Ami ekjon business man";
+var country = "Bangladesh";
+
+// Boolean type variable
+var isRich = true;
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof country);
+
+// Number type variable
+var price = 555;
+console.log(typeof price);
