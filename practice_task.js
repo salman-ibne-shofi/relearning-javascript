dcb59dc2aa35e1e2fb26365277f1ed834d263200
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-02-15 14:36:21 +0600

    practice task

---
+++ practice_task.js
@@ -0,0 +1,41 @@
+// practice task 1
+// var pocket_money = 1000;
+
+// var orange_per_kg = 200;
+// var apple_per_kg = 350;
+// var total_price = orange_per_kg + apple_per_kg;
+// console.log(total_price);
+
+// var return_tk = pocket_money - total_price;
+// console.log(return_tk);
+
+// practice task 2
+// var mathematics = 75.25;
+// var biology = 65;
+// var chemistry = 80;
+// var physics = 35.45;
+// var bangla = 99.50;
+
+// var total_subject = 5;
+
+// var total_marks = mathematics + biology + chemistry + physics + bangla;
+// console.log(total_marks);
+
+// var average_marks = total_marks / total_subject;
+// console.log(average_marks);
+
+// console.log(average_marks.toFixed(2));
+
+// practice task 3
+// var sum1 = 119;
+// var sum2 = 5;
+
+// var remainder = sum1 % sum2;
+// console.log(remainder);
+
+// practice task 4
+// const a = isNaN('11');
+// console.log(a);
+
+// const a = isNaN(2-10);
+// console.log(a);
